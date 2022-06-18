f7232a3a554b93548dfca725d1bf2ed9787c0c90
Ceavin Rufus <16521304@std.stei.itb.ac.id>
2022-06-18 20:48:53 +0700

    Plan component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 // home page
 
 import BalanceBox from "../components/BalanceBox";
+import Plans from "../components/Plans";
 
 const Home = () => {
   return (
     <>
-      <BalanceBox />
+      {/* <BalanceBox /> */}
+      <Plans />
     </>
   );
 };
