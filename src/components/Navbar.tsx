4816d102cdc91548e9bccd121b63268732a729d9
Ceavin Rufus <16521304@std.stei.itb.ac.id>
2022-06-18 18:10:54 +0700

    balance box component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,24 @@
-import React from "react";
-import MenuIcon from '@mui/icons-material/Menu';
+import React, { useState } from "react";
+import "./Navbar.css"
+import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa"
 
 export const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const closeMobileMenu = () => setClick(false);
+
   return (
-    <nav>
-      <div className="flex m-auto justify-center w-[32rem]">
-        <div className=""><MenuIcon /></div>
-        <div className="bg-black text-white w-[32rem]">
-          <h1 className="text-right">Halo, Hekpes</h1>
+    <>
+      <nav className="navbar ">
+        <div className="flex px-10 bg-black m-auto justify-center w-[32rem] h-[80px] bg-[#ec7251] items-center">
+
+          <div className="text-white" onClick={() => setClick(!click)}>{click ? <FaTimes /> : <FaBars />}</div>
+          <div className=" text-white w-[32rem]">
+            <h1 className="text-right">Halo, Hekpes</h1>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav >
+    </>
+
   );
 };
