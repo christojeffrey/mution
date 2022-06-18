5ed37b83b31510ade409246ed5923a8230e5171a
Ceavin Rufus <16521304@std.stei.itb.ac.id>
2022-06-19 03:47:49 +0700

    styling allmutations, add wallet

--- src/pages/AllMutations.tsx
+++ src/pages/AllMutations.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import { useGetMutation } from "../utils/brick";
 import { getAllUserAccessToken, getConnectedWallets } from "../utils/database";
+import { BsFillCalendarFill } from "react-icons/bs"
+
+import MutationCard from "../components/MutationCard";
 
 const AllMutations = () => {
   const uats = getAllUserAccessToken("user1");
@@ -11,7 +14,13 @@ const AllMutations = () => {
     setMutationsData(data);
   });
   return (
-    <div>
+    <div className="mutation-container">
+      <div className="w-[27rem] mt-10">
+        <h1 className='font-bold text-3xl text-[#474646] mb-5'>Mutasi Gabungan</h1>
+        <h1 className='text-sm text-[#474646]'>Lihat mutasi gabungan harianmu! Rencanakan keuanganmu dengan mudah </h1>
+        <br />
+        <div className="flex items-center bg-[#D9D9D9] text-[#5689F5] font-bold w-fit bg-opacity-20 mb-5"><div className="mr-2 font-bold"><BsFillCalendarFill /></div>18/06 - 18/06</div>
+      </div>
       {mutationsData?.map((wallet: any, index: any) => {
         return wallet.data.map((record: any) => {
           console.log("record");
@@ -24,7 +33,7 @@ const AllMutations = () => {
             name = connectedWalletInfo[index].name;
           }
           return (
-            <div>
+            <div className="">
               <MutationCard data={record} id={id} name={name} />
             </div>
           );
@@ -34,16 +43,5 @@ const AllMutations = () => {
   );
 };
 
-const MutationCard = ({ data, id, name }: { data: any; id: any; name: any }) => {
-  console.log("data tiap mutation card");
-  console.log(data);
-  return (
-    <div className="flex">
-      <div>{name}</div>
-      <div>{data.description}</div>
-      <div>{data.direction}</div>
-      <div>{data.amount}</div>
-    </div>
-  );
-};
+
 export default AllMutations;
