70f82c713a070009b63ad519e0ab270ce765a786
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2022-06-18 20:48:43 +0700

    home sekarang lebih terstruktur

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,10 @@
 // home page
 
 import { useEffect, useState } from "react";
-import { getAllUserAccessToken } from "../utils/database";
+import { getAllUserAccessToken, getConnectedWallets } from "../utils/database";
 import { useGetAllWalletData } from "../utils/brick";
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
@@ -19,7 +21,13 @@ const Home = () => {
       </div>
       {/* home hero */}
       {/* connected wallet */}
-      <div>connected wallet and it's balance</div>
+      <div>
+        connected wallet and it's balance
+        <Button variant="text">
+          <Link to="/add">add more wallet</Link>
+        </Button>
+        <AllConnectedWallets />
+      </div>
       {/* connected wallet */}
     </div>
   );
@@ -27,6 +35,7 @@ const Home = () => {
 
 const HomeHero = () => {
   const [totalBalance, setTotalBalance] = useState<any>(0);
+
   const uats = getAllUserAccessToken("user1");
   const [dataWallets, setDataWallets] = useState<any>([]);
   useGetAllWalletData(uats).then((data) => {
@@ -51,4 +60,44 @@ const HomeHero = () => {
 
   return <div>ini hero yang ada di home. disini ditampilin uang total saat ini.{totalBalance}</div>;
 };
+
+const AllConnectedWallets = () => {
+  const uats = getAllUserAccessToken("user1");
+  const [dataWallets, setDataWallets] = useState<any>([]);
+  useGetAllWalletData(uats).then((data) => {
+    setDataWallets(data);
+  });
+  return (
+    <div>
+      {dataWallets?.map((wallet: any, index: any) => {
+        let total = 0;
+        wallet.data.forEach((data: any) => {
+          total += data.balances.available;
+        });
+        console.log("total");
+        console.log(total);
+        console.log("wllet");
+        console.log(wallet);
+        let connectedWalletInfo = getConnectedWallets("user1");
+        let id;
+        let name;
+        if (connectedWalletInfo) {
+          id = connectedWalletInfo[index].id;
+          name = connectedWalletInfo[index].name;
+        }
+        console.log("aAAA");
+        return <ConnectedWalletCard wallet={{ id, name, total }} />;
+      })}
+    </div>
+  );
+};
+const ConnectedWalletCard = ({ wallet }: { wallet: any }) => {
+  return (
+    <div>
+      <div className="text-xl">{wallet.name}</div>
+      <div className="text-xl">{wallet.total}</div>
+    </div>
+  );
+};
+
 export default Home;
