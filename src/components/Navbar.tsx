3e8f89ec45a1ce48f7cb68092f46a837ecf56e27
Ceavin Rufus <16521304@std.stei.itb.ac.id>
2022-06-18 13:53:01 +0700

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,15 @@
 import React from "react";
+import MenuIcon from '@mui/icons-material/Menu';
 
 export const Navbar = () => {
   return (
-    <>
-      <div className="bg-black ">
-        <div className="text-white">Halo, ....</div>
-        <div className=""></div>
+    <nav>
+      <div className="flex m-auto justify-center w-[32rem]">
+        <div className=""><MenuIcon /></div>
+        <div className="bg-black text-white w-[32rem]">
+          <h1 className="text-right">Halo, Hekpes</h1>
+        </div>
       </div>
-    </>
+    </nav>
   );
 };
