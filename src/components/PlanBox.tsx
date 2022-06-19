3014828b3465221bcb7f78f838e730593893d2e9
Ceavin Rufus <16521304@std.stei.itb.ac.id>
2022-06-19 16:03:03 +0700

    styling plan

--- src/components/PlanBox.tsx
+++ src/components/PlanBox.tsx
@@ -1,21 +1,44 @@
 import React, { useState } from "react";
-import "./PlanBox.css"
+import "./PlanBox.css";
 import ProgressBar from "./ProgressBar";
 
-const PlanBox = () => {
-    const [progress, setProgress] = useState(10);
+const PlanBox = (props: { category: any; progress: any; initial: any, color: any }) => {
+    // const [progress, setProgress] = useState(10);
+    const { category, progress, initial, color } = props;
 
     return (
-        <div className="flex flex-col justify-content items-center">
+        <div className="flex flex-col justify-content items-center mt-10">
             <div className="items-center flex flex-col p-10">
-                <h1 className="text-[#5689F5] font-bold text-2xl">Transportasi</h1>
-                <ProgressBar color="#F7CE50" strokeWidth={20} fontsize={25} size={150} progress={progress} />
-                <h3 className="font-bold">Rp125.000</h3>
-                <h3>/Rp250.000</h3>
+                <h1 className="text-[#5689F5] mb-5 font-bold text-2xl">{category}</h1>
+                <ProgressBar
+                    color={color}
+                    strokeWidth={20}
+                    fontsize={"2xl"}
+                    size={150}
+                    progress={progress}
+                />
+                <h3 className="font-bold mt-5 text-3xl">
+                    Rp
+                    {(initial * progress * 0.01)
+                        .toString()
+                        .replace(/\B(?=(\d{3})+(?!\d))/g, ".")}
+                </h3>
+                <h3>/Rp{initial.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")}</h3>
                 <br />
 
                 <p className="text-xs">Sisa Dana</p>
-                <h3 className="flex px-2 text-[#a7ca5a] font-bold text-xl">Rp125.000</h3>
+                {initial * progress * 0.01 > initial ? (
+                    <h3 className="flex px-2 text-[#a7ca5a] font-bold text-3xl">
+                        Rp0
+                    </h3>
+                ) : (
+                    <h3 className="flex px-2 text-[#a7ca5a] font-bold text-3xl">
+                        Rp
+                        {(initial - initial * progress * 0.01)
+                            .toString()
+                            .replace(/\B(?=(\d{3})+(?!\d))/g, ".")}
+                    </h3>
+                )}
             </div>
         </div>
     );
--- src/pages/Planning.tsx
+++ src/pages/Planning.tsx
@@ -1,14 +1,34 @@
 import React from "react";
 import PlanBox from "../components/PlanBox";
 
+// import Swiper core and required modules
+import { Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
 const Planning = () => {
     return (
-        <div className="flex flex-col">
-            <div className="w-[27rem] mt-10">
+        <div className="flex flex-col ">
+            <div className="w-[27rem] mt-10 mx-10">
                 <h1 className="font-bold text-3xl text-[#474646] mb-5">Perencanaan dana</h1>
                 <h1 className="text-sm text-[#474646]">Lihat dan rencanakan keuanganmu dalam satu bulan!</h1>
             </div>
-            <PlanBox />
+            <div className="">
+                <Swiper
+                    pagination={{
+                        dynamicBullets: true,
+                        clickable: true
+                    }}
+                    modules={[Pagination]}
+                >
+                    <SwiperSlide><PlanBox progress={50} initial={250000} category="Transportasi" color="#8A81E4" /></SwiperSlide>
+                    <SwiperSlide><PlanBox progress={120} initial={500000} category="Makanan" color="#FF6240" /></SwiperSlide>
+                    <SwiperSlide><PlanBox progress={75} initial={250000} category="Lainnya" color="#F7CE50" /></SwiperSlide>
+                </Swiper>
+            </div>
         </div>
     );
 };
