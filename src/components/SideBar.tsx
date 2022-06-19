89e30f1db4dabf4581a3a22869f7ee00c29f492b
Ceavin Rufus <16521304@std.stei.itb.ac.id>
2022-06-19 18:00:51 +0700

    change navbar color

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SideBar = () => {
+    return (
+        <div className='h-screen  bg-[#F4F7FB]  w-full h-[40rem]'>SideBar</div>
+    )
+}
+
+export default SideBar
